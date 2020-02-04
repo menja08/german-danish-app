227ec4fc50fdf52f97f78e00534d8a3f3b76134c
ivan <menja08@yahoo.com>
2020-02-05 00:19:51 +0100

    corrected indentation, removed print statements, unnecessary comments

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.use("/create", (req, res) => {
 
 	// if the request body is empty {}
 	var wordsToBeSaved = Object.values(req.body);
-	//console.log("wordsToBeSaved is the Array = " + wordsToBeSaved);
 
 	// before saving, check inputs
 	if ((wordsToBeSaved.includes("")) || (wordsToBeSaved.includes(null)) || (wordsToBeSaved.includes(undefined))) {
@@ -35,7 +34,7 @@ app.use("/create", (req, res) => {
 	    // read from database to avoid duplicates
 	    Word.find({german : req.body.german}, (err) => {
 		if (err) {
-		    // err if the word is not in database
+		    // error if the word is not in database
 		    // then save
 		    newWord.save((err, word) => {
 			if (err) {
@@ -52,7 +51,6 @@ app.use("/create", (req, res) => {
 	    });
 	}
     }
-    
 });
 
 // read (CRUD) 1 of 2
@@ -63,19 +61,15 @@ app.use("/showLastCreated", (req, res) => {
 	    res.type().status(500);
 	    res.send("Error " + err);
 	} else {
-	    //console.log(word[word.length-1]);
 	    last = word[word.length-1];
-	    //res.json(word);
 	    res.json(last);
 	}
-	});
-    //res.redirect("/files/lastCreated.html");
+    });
 });
 
 // read (CRUD) 2 of 2
 app.use("/showAllWords", (req, res) => {
     //get info from database
-    //res.redirect("/files/showAll.html")
     Word.find((err, word) => {
 	if(err) {
 	    res.type().status(500);
@@ -96,13 +90,12 @@ app.use("/update", (req, res) => {
 	    res.send("Error: " + err);
 	} else if (!word) {
 	    res.type('html').status(200);
-	    res.send("No person named " + oldGermanWord);
+	    res.send("No word named " + oldGermanWord);
 	} else {
-	    //res.json(word);
 	    Word.updateOne({german:oldGermanWord}, {german:newGermanWord}, (err, word) => {
 		if (err) {
 		    res.type('html').status(500);
-		    res.send('No person named' + err);
+		    res.send('No word named' + err);
 		} else {
 		    res.json(word);
 		}
@@ -114,7 +107,6 @@ app.use("/update", (req, res) => {
 // delete (CRUD) 1 of 2
 app.use("/deleteWord", (req, res) => {
     var query = req.body.german;
-    console.log("query in 1: " + query)
 
     // check if word is in database anyway
     Word.find({german: query}, (err, word) => {
@@ -122,9 +114,6 @@ app.use("/deleteWord", (req, res) => {
 	    console.log(err);
 	    res.type().status(500);
 	} else {
-	    //var germanWord = res.json(word);
-	    //console.log(germanWord);
-	    
 	    Word.deleteOne({german: query}, (err) => {
 		if (err) {
 		    res.type().status(500);
@@ -132,13 +121,10 @@ app.use("/deleteWord", (req, res) => {
 		} else {
 		    // search the word (again)
 		    Word.findOne({german:query}, (err, word) => {
-			console.log("query in 2:" + query);
 			if (err) {
-			    console.log(err);
 			    res.type().status(500);
 			    res.send("Err " + err);
 			} else {
-			    //res.json(word);
 			    res.redirect("/files/home.html");
 			}
 		    });
@@ -146,31 +132,10 @@ app.use("/deleteWord", (req, res) => {
 	    });
 	}
     });
-    /*
-    Word.deleteOne({german: query}, (err) => {
-	if (err) {
-	    res.type().status(500);
-	    res.send("Error " + err);
-	} else {
-	    // search the word (again)
-	    Word.findOne({german:query}, (err, word) => {
-		console.log("query in 2:" + query);
-		if (err) {
-		    console.log(err);
-		    res.type().status(500);
-		    res.send("Err " + err);
-		} else {
-		    res.json(word);
-		}
-	    });
-	}
-    });
-    */
 });
 
 app.use("/search", (req, res) => {
-    // takes a query
-    console.log("the request is = " + req.body.german);
+    
     Word.find({german: req.body.german}, (err, words) => {
 	if (err) {
 	    res.type().status(500);
@@ -184,17 +149,17 @@ app.use("/search", (req, res) => {
 		res.redirect("/files/errorPage.html");
 	    } else {
 		res.render('search', {
-		german: mywords.german,
-		danish: mywords.danish,
-		english: mywords.english
-		});}
+		    german: mywords.german,
+		    danish: mywords.danish,
+		    english: mywords.english
+		});
+	    }
 	}
     });
 });
 
 app.use(express.static(__dirname));
 app.use("/public", express.static("files"));
-//app.use('/favicon.ico', express.static('files/images/favicon.ico'));
 
 // javascript redirect
 app.use("/js", express.static('js'));
@@ -202,12 +167,8 @@ app.use("/js", express.static('js'));
 
 //home redirect
 app.use("/", (req, res) => {
-    //res.redirect("/files/enterWords.html");
     res.redirect("/files/home.html");
 });
-
-//app.use();
-
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
