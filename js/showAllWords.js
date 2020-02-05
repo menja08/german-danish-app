a0704cd894e8114a81862c4d8aaaf4148dc569df
ivan <menja08@yahoo.com>
2020-02-05 16:00:06 +0100

    added boxDiv that encloses the 3 words

--- js/showAllWords.js
+++ js/showAllWords.js
@@ -51,20 +51,29 @@ function addWords2(words, showAllWords2) {
 	var germanWord = words[i].german;
 	var danishWord = words[i].danish;
 	var englishWord = words[i].english;
+
+	var boxDiv = document.createElement("div");
+	boxDiv.style.border = "1px solid green";
 	
 	var germanDiv = document.createElement("div");
 	var danishDiv = document.createElement("div");
 	var englishDiv = document.createElement("div");
 	
 	germanDiv.append(germanWord);
-	showAllWords2.append(germanDiv);
+	//showAllWords2.append(germanDiv);
+	boxDiv.append(germanDiv);
+	showAllWords2.append(boxDiv);
 
 	danishDiv.append(danishWord);
-	showAllWords2.append(danishDiv);
+	//showAllWords2.append(danishDiv);
+	boxDiv.append(danishDiv);
+	showAllWords2.append(boxDiv);
 	danishDiv.style.backgroundColor = "orange";
 
 	englishDiv.append(englishWord);
-	showAllWords2.append(englishDiv);
+	//showAllWords2.append(englishDiv);
+	boxDiv.append(englishDiv);
+	showAllWords2.append(boxDiv);
 	
 	console.log(showAllWords2)
 
