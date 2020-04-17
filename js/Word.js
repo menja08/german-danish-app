58d8373d7e59b91cf6c94ae08d0c36758662b8ca
ivan <menja08@yahoo.com>
2020-04-17 18:18:03 +0200

    changed database connection string

--- js/Word.js
+++ js/Word.js
@@ -2,7 +2,8 @@
 var mongoose = require('mongoose');
 //mongoose.connect("mongodb://localhost:27017/german-danish",{useNewUrlParser:true, useUnifiedTopology:true});
 
-mongoose.connect("mongodb://menja08:kakeeto88@ds259878.mlab.com:59878/heroku_fzg6t17c",{useNewUrlParser:true, useUnifiedTopology:true});
+
+mongoose.connect(PROD_MONGODB, {useNewUrlParser:true, useUnifiedTopology:true});
 
 var Schema = mongoose.Schema;
 
