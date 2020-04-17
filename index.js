e220766e55e674cbc276b208fd6d3b3691b1919a
ivan <menja08@yahoo.com>
2020-04-17 05:50:03 +0200

    now listening on port 8000

--- index.js
+++ index.js
@@ -175,6 +175,6 @@ app.use("/", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+app.listen(8000, () => {
+    console.log("Listening on port 8000");
 });
