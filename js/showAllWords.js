b989f0952b15c8d6e55896421c31a8bd3f9acc56
ivan <menja08@yahoo.com>
2020-04-17 22:21:37 +0200

    jquery network error not resolved

--- js/showAllWords.js
+++ js/showAllWords.js
@@ -7,8 +7,9 @@ var numberOfWordsId = document.getElementById("numberOfWords");
 // for each key in words, create an element
 var showAllWords2 = document.getElementById("showAllWords2");
 
-//url = "http://localhost:3000/showAllWords";
+// url = "http://localhost:3000/showAllWords";
 let url = window.location.origin + "/showAllWords";
+console.log("url1 = " + url);
 
 $.getJSON(url, (words, status) => {
 
