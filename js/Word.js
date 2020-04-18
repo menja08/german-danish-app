49be8ee7238d0bbeb5d4592f05271f7dab54774c
ivan <menja08@yahoo.com>
2020-04-18 15:26:34 +0200

    added MONGODB_URI

--- js/Word.js
+++ js/Word.js
@@ -4,7 +4,12 @@ var mongoose = require('mongoose');
 
 //mongodb+srv://menja08:<password>@cluster0-busdh.mongodb.net/test?retryWrites=true&w=majority
 
-mongoose.connect(process.env."mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/german-danish?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology:true});
+//mongoose.connect(process.env."mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/german-danish?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology:true});
+
+uriCloud = process.env.MONGODB_URI;
+uriLocal = "mongodb://localhost:27017/german-danish";
+
+mongoose.connect(uriCloud || uriLocal, {useNewUrlParser:true, useUnifiedTopology:true});
 
 var Schema = mongoose.Schema;
 
