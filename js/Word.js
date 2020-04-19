f7e2f1b7888040c9938b4173212ca06f03a0e63d
ivan <menja08@yahoo.com>
2020-04-19 03:09:41 +0200

    removed port because of srv string

--- js/Word.js
+++ js/Word.js
@@ -7,12 +7,14 @@ var mongoose = require('mongoose');
 //mongoose.connect(process.env."mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/german-danish?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology:true});
 
 //uriCloud = process.env.MONGODB_URI;
-uriCloud = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net:27017/german-danish?retryWrites=true&w=majority";
+uriCloud = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/german-danish?retryWrites=true&w=majority";
 // cluster0-shard-00-01-busdh.mongodb.net:27017
 uriCloud2 = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net:27017/test?retryWrites=true&w=majority";
 uriLocal = "mongodb://localhost:27017/german-danish";
 
-mongoose.connect(uriCloud || uriCloud2 || uriLocal, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(uriCloud || uriCloud2 || uriLocal, {useNewUrlParser:true, useUnifiedTopology:true}).catch((err) => {
+    console.log("There is an ERROR: " + err);
+});
 
 var Schema = mongoose.Schema;
 
