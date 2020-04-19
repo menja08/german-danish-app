552f2b568ab9bf2787ed02d2adf169ae9b96058d
ivan <menja08@yahoo.com>
2020-04-19 14:01:28 +0200

    removed unnecessary connection strings

--- js/Word.js
+++ js/Word.js
@@ -1,18 +1,8 @@
-// defines a schema and model slide 512
+// defines a schema and model
 require('dotenv').config();
 var mongoose = require('mongoose');
-// mongoose.connect("mongodb://localhost:27017/german-danish",{useNewUrlParser:true, useUnifiedTopology:true});
-
-//mongodb+srv://menja08:<password>@cluster0-busdh.mongodb.net/test?retryWrites=true&w=majority
-
-//mongoose.connect(process.env."mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/german-danish?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology:true});
 
 uriCloud = process.env.MONGODB_URI;
-
-//uriCloud = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/german-danish";
-
-// cluster0-shard-00-01-busdh.mongodb.net:27017
-//uriCloud2 = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/test?retryWrites=true&w=majority";
 uriLocal = "mongodb://localhost:27017/german-danish";
 
 mongoose.connect(uriCloud || uriLocal, {useNewUrlParser:true, useUnifiedTopology:true}, (err) => {
