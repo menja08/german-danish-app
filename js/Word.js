07e1bcd4538f2cf92f42663cafb89f43cee73c25
ivan <menja08@yahoo.com>
2020-04-19 02:04:15 +0200

    added new string

--- js/Word.js
+++ js/Word.js
@@ -7,10 +7,12 @@ var mongoose = require('mongoose');
 //mongoose.connect(process.env."mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/german-danish?retryWrites=true&w=majority", {useNewUrlParser:true, useUnifiedTopology:true});
 
 //uriCloud = process.env.MONGODB_URI;
-uriCloud = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net:27017/german-danish?retryWrites=true&w=majority"
+uriCloud = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net:27017/german-danish?retryWrites=true&w=majority";
+// cluster0-shard-00-01-busdh.mongodb.net:27017
+uriCloud2 = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/test?retryWrites=true&w=majority";
 uriLocal = "mongodb://localhost:27017/german-danish";
 
-mongoose.connect(uriCloud || uriLocal, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(uriCloud2 || uriCloud || uriLocal, {useNewUrlParser:true, useUnifiedTopology:true});
 
 var Schema = mongoose.Schema;
 
