3713ba893eb18de0e168f486950a1460fedb2e9b
ivan <menja08@yahoo.com>
2020-04-19 02:36:03 +0200

    changed whitelist in atlas, connection string

--- js/Word.js
+++ js/Word.js
@@ -9,10 +9,10 @@ var mongoose = require('mongoose');
 //uriCloud = process.env.MONGODB_URI;
 uriCloud = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net:27017/german-danish?retryWrites=true&w=majority";
 // cluster0-shard-00-01-busdh.mongodb.net:27017
-uriCloud2 = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net/test?retryWrites=true&w=majority";
+uriCloud2 = "mongodb+srv://menja08:kakeeto88@cluster0-busdh.mongodb.net:27017/test?retryWrites=true&w=majority";
 uriLocal = "mongodb://localhost:27017/german-danish";
 
-mongoose.connect(uriCloud2 || uriCloud || uriLocal, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connect(uriCloud || uriCloud2 || uriLocal, {useNewUrlParser:true, useUnifiedTopology:true});
 
 var Schema = mongoose.Schema;
 
